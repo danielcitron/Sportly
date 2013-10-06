a74ed9f0c5ae003a8968cb93688af3359895095f
Daniel Citron <daniel.citron@gmail.com>
2013-10-06 18:54:16 -0400

    new block animation for homepage, various bug fixes, aesthetic changes

--- js/index-ck.js
+++ js/index-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("#search").waypoint("sticky",{offset:75});$("#block-story-1").click(function(){$("#story-loader").addClass("story-loading")});$("#close-button").click(function(){$("#story-loader").removeClass("story-loading")});$("#block-story-1").hover(function(){$("#block-title-container").addClass("title-visible")});$("#block-story-1").mouseout(function(){$("#block-title-container").removeClass("title-visible")})});
+$(document).ready(function(){$("#search").waypoint("sticky",{offset:75})});
--- js/index.js
+++ js/index.js
@@ -4,20 +4,12 @@ $( document ).ready(function() {
     	offset: 75
     });
 
-	$('#block-story-1').click(function() {
-		$('#story-loader').addClass('story-loading');
-	});
-
-	$('#close-button').click(function() {
-		$('#story-loader').removeClass('story-loading');
-	});
-
-	$('#block-story-1').hover(function() {
+/*	$('#block-story-1').hover(function() {
 		$('#block-title-container').addClass('title-visible');
 	});
 
 	$('#block-story-1').mouseout(function() {
 		$('#block-title-container').removeClass('title-visible');
-	});
+	}); */
 
 });
