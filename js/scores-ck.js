d479acd2b1b96ce6e1fce4fd1c290a0beb84df4c
Daniel Citron <daniel.citron@gmail.com>
2013-10-07 04:22:48 -0400

    Score hover effect now is JS instead of pure CSS
    only occurs when on footer, versus full block

--- js/scores-ck.js
+++ js/scores-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$(".score-text").mouseover(function(){$(this).addClass("open");$(this).parent().find(".score-photo").addClass("closed")});$(".score-text").mouseout(function(){$(this).removeClass("open");$(this).parent().find(".score-photo").removeClass("closed")})});
--- js/scores.js
+++ js/scores.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
 
+	$('.score-text').mouseover(function() {
+		$(this).addClass('open');
+		$(this).parent().find('.score-photo').addClass('closed');
+	});
 
+	$('.score-text').mouseout(function() {
+		$(this).removeClass('open');
+		$(this).parent().find('.score-photo').removeClass('closed');
+	});
 	
 });
